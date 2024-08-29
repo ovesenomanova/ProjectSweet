3cdcbec9ab76f7be871ff640e8b0b1c429d2cb6f
EOV <sweet16102019@gmail.com>
2024-08-29 20:45:19 +0400

    - v10

--- _sweetsaratov_/urls.py
+++ _sweetsaratov_/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('forum/', include('apps.forum.urls')),
     path('payment/', include('apps.payment.urls')),
     path('users/', include('apps.users.urls')),
-    path('', TitleViewRedirect.as_view)
+    path('', TitleViewRedirect.as_view())
 
 ]
--- _sweetsaratov_/views.py
+++ _sweetsaratov_/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import RedirectView
 from django.urls import reverse_lazy
 
+
 class TitleViewRedirect(RedirectView):
-    url = reverse_lazy('user_register')
+    url = reverse_lazy('main')
